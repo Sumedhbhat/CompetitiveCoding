147aac476568e440e5daf91f4c7604bebaa92566
Sumedh Bhat <sumedhbhat01@gmail.com>
2022-02-09 23:25:50 +0530

    feb 9th

--- LeetCode/278.first-bad-version.cpp
+++ LeetCode/278.first-bad-version.cpp
@@ -11,7 +11,17 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        
+        long int left=1,right=n,mid=1;
+        while(left<=right){
+            mid=(left+right)/2;
+            if (isBadVersion(mid)){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return right+1;
     }
 };
 // @lc code=end
