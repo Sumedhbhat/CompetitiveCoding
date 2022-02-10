620aff84acf96c3c9894a0d56aa6d08829ec6899
Sumedh Bhat <sumedhbhat01@gmail.com>
2022-02-10 23:51:28 +0530

    feb 10th

--- LeetCode/283.move-zeroes.cpp
+++ LeetCode/283.move-zeroes.cpp
@@ -8,7 +8,21 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int len=nums.size(),count=0;
+        int len=nums.size(),count=0,i=0,len1;
+        len1=len;
+        while(i<(len)){
+            if(nums[i]==0){
+                nums.erase(nums.begin()+i);
+                len--;
+            }else{
+                i++;
+            }
+        }
+        len1-=len;
+        while((len1)!=0){
+            nums.push_back(0);
+            len1--;
+        }
     }
 };
 // @lc code=end
