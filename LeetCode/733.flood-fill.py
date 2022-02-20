e48919d19c92f30d320ff850ee724c068dcc29de
Sumedh Bhat <sumedhbhat01@gmail.com>
2022-02-21 00:19:45 +0530

    feb 21

--- LeetCode/733.flood-fill.py
+++ LeetCode/733.flood-fill.py
@@ -6,14 +6,26 @@
 
 # @lc code=start
 class Solution:
-    visited=[[0]*50]*50
+    
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        self.m=len(image)
-        self.n=len(image[0])
-        traverse={[sr,sc]}
+        m=len(image)
+        n=len(image[0])
+        visited=[[0 for x in range(n)] for y in range(m)]
+        orgColor=image[sr][sc]
+        traverse=[[sr,sc]]
+        if orgColor==newColor: return image
         while(traverse):
             i,j=traverse.pop()
-            
+            if (i==m or j==n) or (i<0 or j<0):
+                continue
+            elif image[i][j]==orgColor and visited[i][j]==0:
+                visited[i][j]=1
+                image[i][j]=newColor
+                newValues=[[i-1,j],[i+1,j],[i,j+1],[i,j-1]]
+                traverse.extend(newValues)
+            else: continue
+        return image
+
 
 # @lc code=end
 
